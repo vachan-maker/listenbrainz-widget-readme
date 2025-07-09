ca9bb1fa6d9d37487dd82349ead3a117a9de37d4
vachan-maker <vachan@tutanota.com>
2025-07-09 16:33:09 +0530

    Changed size again

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -36,7 +36,7 @@ export async function GET(request: Request) {
 function generateSVG(track_name: string, artist_name: string, music_service_name: string) {
     const svg = `<?xml version="1.0" encoding="utf-8"?>
 <svg xmlns="http://www.w3.org/2000/svg" style="fill: rgb(0, 0, 0);">
-  <rect x="1.6" y="1.848" width="95.88" height="95.88" style="stroke: rgb(0, 0, 0); fill: rgb(51, 51, 51);"/>
+  <rect x="1.6" y="1.848" width="150" height="150" style="stroke: rgb(0, 0, 0); fill: rgb(51, 51, 51);"/>
   <text style="fill: yellow; font-family: &quot;Arial&quot;, sans-serif; font-size: 28px; font-style: italic; font-weight: 700; text-transform: lowercase; white-space: pre;" transform="matrix(0.238591, 0, 0, 0.22727, 30.100395, 17.420271)" x="6.822999954223633" y="57.96200180053711">listening to</text>
   <text style="fill: rgb(33, 192, 99); font-family: &quot;Arial&quot;, sans-serif; font-size: 28px; font-style: italic; font-weight: 700; text-anchor: middle; white-space: pre;" transform="matrix(0.352341, 0, 0, 0.497911, 47.136147, 21.40827)" x="6.823" y="57.962">${track_name}</text>
   <text style="fill: rgb(33, 192, 99); font-family: &quot;Arial&quot;, sans-serif; font-size: 28px; font-style: italic; font-weight: 700; text-transform: lowercase; white-space: pre;" transform="matrix(0.238591, 0, 0, 0.22727, 25.265982, 53.140553)" x="6.822999954223633" y="57.96200180053711">by ${artist_name}</text>
@@ -51,7 +51,7 @@ function generateSVG(track_name: string, artist_name: string, music_service_name
 function generateNotPlaying() {
         const svg = `<?xml version="1.0" encoding="utf-8"?>
 <svg xmlns="http://www.w3.org/2000/svg" style="fill: rgb(0, 0, 0);">
-  <rect x="1.6" y="1.848" width="50" height="50" style="stroke: rgb(0, 0, 0); fill: rgb(51, 51, 51);"/>
+  <rect x="1.6" y="1.848" width="150" height="150" style="stroke: rgb(0, 0, 0); fill: rgb(51, 51, 51);"/>
   <text style="fill: yellow; font-family: &quot;Arial&quot;, sans-serif; font-size: 28px; font-style: italic; font-weight: 700; text-transform: lowercase; white-space: pre;" transform="matrix(0.238591, 0, 0, 0.22727, 30.100395, 17.420271)" x="6.822999954223633" y="57.96200180053711">I am probably sleeping or something</text>
   <text style="fill: rgb(33, 192, 99); font-family: &quot;Arial&quot;, sans-serif; font-size: 28px; font-style: italic; font-weight: 700; text-anchor: middle; white-space: pre;" transform="matrix(0.352341, 0, 0, 0.497911, 47.136147, 21.40827)" x="6.823" y="57.962">Not Listening</text>
   <text style="fill: rgb(33, 192, 99); font-family: &quot;Arial&quot;, sans-serif; font-size: 28px; font-style: italic; font-weight: 700; text-transform: lowercase; white-space: pre;" transform="matrix(0.238591, 0, 0, 0.22727, 25.265982, 53.140553)" x="6.822999954223633" y="57.96200180053711"></text>
